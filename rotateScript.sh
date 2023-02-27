11427458d5398b36d79840128c9eaadedbdbfb7b
Vincent Ducros <vincent.ducros@etu.univ-grenoble-alpes.fr>
2023-02-27 15:07:02 +0100

    Modified rotateScript to also modify digitizer direction

--- rotateScript.sh
+++ rotateScript.sh
@@ -2,21 +2,27 @@ orientation=$(xrandr -q --verbose | grep eDP-1 | sed 's/primary //' | awk '{prin
 case $orientation in
   normal)
     xrandr -o left
+    coords="0 -1 1 1 0 0 0 0 1"
     ;;
 
   left)
     xrandr -o inverted
+    coords="-1 0 1 0 -1 1 0 0 1"
     ;;
 
   inverted)
     xrandr -o right
+    coords="0 1 0 -1 0 1 0 0 1"
     ;;
 
   right)
     xrandr -o normal
+    coords="0 0 0 0 0 0 0 0 0"
     ;;
   *)
   	echo -n "Orientation inconnue"
   	;;
 esac
 
+xinput set-prop "eGalaxTouch Virtual Device for Touch" --type=float "Coordinate Transformation Matrix" $coords #modify this line with your own touch driver
+xinput set-prop "eGalaxTouch Virtual Device for Single" --type=float "Coordinate Transformation Matrix" $coords
